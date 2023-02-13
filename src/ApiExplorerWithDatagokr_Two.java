28c5cf429574225bd9aa1cc78aa462fa80a57bb5
Giljinsu <therin234@gmail.com>
2023-02-13 13:34:51 +0900

    11

--- src/ApiExplorerWithDatagokr_Two.java
+++ src/ApiExplorerWithDatagokr_Two.java
@@ -19,7 +19,8 @@ public class ApiExplorerWithDatagokr_Two {
         */
 
         
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List"); 
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List");
+        // https로 보내면 javax.net.ssl.SSLHandshakeException 발생
         // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=").append("g%2FF2l2m%2FAvNycT8bXjhCgioX0PwrxgQAzt5gL3DFxLBXzwpTXyHZ%2FyFZqGy7OH14DFXaHvf0PN5PD92Nu8IV8A%3D%3D");
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
