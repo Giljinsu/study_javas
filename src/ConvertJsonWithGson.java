a7998998cbcec5821ff3a4db63069010a0222180
Giljinsu <therin234@gmail.com>
2023-02-13 15:22:17 +0900

    11

---
+++ src/ConvertJsonWithGson.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+import beans.MemberBean;
+
+public class ConvertJsonWithGson {
+    //문자열 Json
+    public static void main(String[] args) {
+        // 문자열 Json -> Map
+        Gson gson = new Gson();
+        String jsonStr = "{id: 1, name: jinsu}";
+        Map jsonMap = gson.fromJson(jsonStr, Map.class); //자기가 바꾸려는 형태 Map.class를 넣음
+        
+        // Map -> 문자열 Json
+        String mapStr = gson.toJson(jsonMap);
+        
+        // 문자열 Json -> Object(bean, model, vo)
+        String jsonStrBean = "{firstName: gil, secondName: jin, handleName:su}";
+        MemberBean bean = gson.fromJson(jsonStrBean, MemberBean.class);
+        int i = 9;
+    }
+}
